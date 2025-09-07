8437d59f720219b147aa6b2dd11c4c8b079243fd
Hanna05136 <hhannahernandez21@gmail.com>
2025-09-07 18:03:37 -0500

    Primer commit

---
+++ demo/src/main/java/com/poryectojpa/demo/HannaApplication.java
@@ -0,0 +1,14 @@
+package com.poryectojpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HannaApplication.class, args);
+
+    }
+
+}
